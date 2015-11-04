5fd120f3edeeafd1513744f4ba2cf9bbfdd7f5d4
Gaël Dupont <gaeldupont44@gmail.com>
2015-11-04 23:31:57 +0100

    using directive for rectangle of viewer's name

--- public/app/SocialCtrl.js
+++ public/app/SocialCtrl.js
@@ -23,53 +23,13 @@ angular
     SocketIOService.onMessagesUpdated(onMessagesUpdated.bind(this));
 
     function onViewersUpdated(viewers) {
-      //Element a gérer avec des directives
-      var taField = document.getElementById("LGDtext");
-      var viewersRect = document.getElementsByClassName("viewers");
-
-      for(var i= 0; i < viewersRect.length; i++){
-        viewersRect[i].parentNode.removeChild(viewersRect[i]);
-      }
-      for(var i = 0 ; i < viewers.length ; i++){
-        if(viewers[i].cursorPosition > -1) {
-          var coordinates = getCaretCoordinates(taField, viewers[i].cursorPosition);
-          //console.log(viewers[i].nickname, coordinates.top, coordinates.left);
-          var rect = document.createElement('div');
-          rect.setAttribute("class", "viewers");
-          rect.innerHTML = viewers[i].nickname;
-          document.body.appendChild(rect);
-          rect.style.textAlign = 'center';
-          rect.style.position = 'absolute';
-          rect.style.color = 'white';
-          rect.style.backgroundColor = 'navy';
-          rect.style.opacity = '0.75';
-          rect.style.padding = '5px 10px';
-          rect.style.top = taField.offsetTop
-              - taField.scrollTop
-              + coordinates.top - rect.offsetHeight
-              + 'px';
-            rect.style.left = taField.offsetLeft
-              - taField.scrollLeft
-              + coordinates.left - rect.offsetWidth/2
-              + 'px';
-          var caret = document.createElement('div');
-          rect.appendChild(caret);
-          caret.style.position = 'absolute';
-          caret.style.backgroundColor = 'navy';
-          caret.style.height = getComputedStyle(taField).getPropertyValue('font-size');
-          caret.style.width = '1px';
-          caret.style.top = rect.offsetHeight + 'px';
-          caret.style.left = rect.offsetWidth/2 + 'px';
-          }
-        }
       this.viewers = viewers;
       $scope.$apply();
     }
 
     SocketIOService.onViewersUpdated(onViewersUpdated.bind(this));
-    //if someone write in the file
+    //if a viewer write the file
     function onLGDUpdated(file) {
-
       this.fileName = file.name;
       this.text = file.text;
       //Recover the focus element
@@ -107,4 +67,59 @@ angular
       );
     }
   };
+}).directive("rectViewers", function(){
+  return {
+    scope: {
+      viewers: "=viewers"
+    },
+    link: function(scope, element, attrs) {
+
+      scope.$watch(
+        function () {
+          return scope.viewers;
+        },
+        function (viewers) {
+          //remove all previous rectangle created
+          var viewersRect = angular.element(document.getElementsByClassName("viewers"));	
+          for(var i= 0; i < viewersRect.length; i++){
+            viewersRect[i].parentNode.removeChild(viewersRect[i]);
+          }
+          //for all viewers
+          for(var i = 0 ; i < viewers.length ; i++){
+            //if viewer is focusing textarea
+            if(viewers[i].cursorPosition > -1) {
+              var coordinates = getCaretCoordinates(element[0], viewers[i].cursorPosition);
+              console.log(viewers[i].nickname, coordinates.top, coordinates.left);
+              var rect = document.createElement('div');
+              rect.setAttribute("class", "viewers");
+              rect.innerHTML = viewers[i].nickname;
+              document.body.appendChild(rect);
+              rect.style.textAlign = 'center';
+              rect.style.position = 'absolute';
+              rect.style.color = 'white';
+              rect.style.backgroundColor = 'navy';
+              rect.style.opacity = '0.75';
+              rect.style.padding = '5px 10px';
+              rect.style.top = element[0].offsetTop
+                - element[0].scrollTop
+                + coordinates.top - rect.offsetHeight
+                + 'px';
+              rect.style.left = element[0].offsetLeft
+                - element[0].scrollLeft
+                + coordinates.left - rect.offsetWidth/2
+                + 'px';
+              var caret = document.createElement('div');
+              rect.appendChild(caret);
+              caret.style.position = 'absolute';
+              caret.style.backgroundColor = 'navy';
+              caret.style.height = getComputedStyle(element[0]).getPropertyValue('font-size');
+              caret.style.width = '1px';
+              caret.style.top = rect.offsetHeight + 'px';
+              caret.style.left = rect.offsetWidth/2 + 'px';
+            }
+          }
+        }, true
+      )
+    }
+  }
 });
